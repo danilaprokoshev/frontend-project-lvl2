d627ad2638af31e5156116dbdd2f98d94db24c21
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-01-06 20:20:50 +0300

    updated bin/gendiff.js on step#2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ const program = new Command();
 
 program
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
