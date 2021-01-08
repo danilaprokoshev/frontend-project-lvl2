e3f5b205669b282e8737543b0d592828b9647b02
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-01-08 18:11:12 +0300

    updated bin/gendiff.js on step#3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import path from 'path';
+import fs from 'fs';
+import genDiff from "../src/gendiff.js";
 
 const program = new Command();
 
@@ -7,6 +10,13 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .action(function (filepath1, filepath2) {
+    const absolutePath1 = path.resolve(filepath1);
+    const absolutePath2 = path.resolve(filepath2);
+    const data1 = fs.readFileSync(absolutePath1);
+    const data2 = fs.readFileSync(absolutePath2);
+    console.log(genDiff(data1, data2));
+  });
 
 program.parse(process.argv);
