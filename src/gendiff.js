b8e39dfcb8c0a2bcb07df15ca8b92fa8941d5d74
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-01-11 11:04:17 +0300

    updated src/gendiff.js on step#4

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,44 +1,31 @@
 import _ from 'lodash';
 
 const genDiff = (data1, data2) => {
+  if (data1 === '{}' && data2 === '{}') {
+    return '{}';
+  }
   const obj1 = JSON.parse(data1);
   const obj2 = JSON.parse(data2);
   const keys1 = _.keys(obj1);
   const keys2 = _.keys(obj2);
   const keys = _.union(keys1, keys2);
 
-  const result = {};
-  for (const key of keys) {
-    if (!_.has(obj1, key)) {
-      result[key] = 'added';
-    } else if (!_.has(obj2, key)) {
-      result[key] = 'deleted';
-    } else if (obj1[key] !== obj2[key]) {
-      result[key] = 'changed';
-    } else {
-      result[key] = 'unchanged';
-    }
-  }
+  const differences = keys
+    .sort()
+    .reduce((acc, key) => {
+      if (!_.has(obj1, key)) {
+        return acc.concat('\n', `  + ${key}: ${obj2[key]}`);
+      }
+      if (!_.has(obj2, key)) {
+        return acc.concat('\n', `  - ${key}: ${obj1[key]}`);
+      }
+      if (obj1[key] !== obj2[key]) {
+        return acc.concat('\n', `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`);
+      }
+      return acc.concat('\n', `    ${key}: ${obj1[key]}`);
+    }, '');
 
-  const keysOfResult = _.keys(result);
-  keysOfResult.sort();
-  let stringifiedResult = '';
-  for (const key of keysOfResult) {
-    if (result[key] === 'added') {
-      stringifiedResult = `${stringifiedResult}\n  + ${key}: ${obj2[key]}`;
-    }
-    if (result[key] === 'deleted') {
-      stringifiedResult = `${stringifiedResult}\n  - ${key}: ${obj1[key]}`;
-    }
-    if (result[key] === 'changed') {
-      stringifiedResult = `${stringifiedResult}\n  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
-    }
-    if (result[key] === 'unchanged') {
-      stringifiedResult = `${stringifiedResult}\n    ${key}: ${obj1[key]}`;
-    }
-  }
-
-  return `{${stringifiedResult}\n}`;
+  return `{${differences}\n}`;
 };
 
 export default genDiff;
