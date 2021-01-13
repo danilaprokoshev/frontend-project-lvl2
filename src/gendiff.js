4c9c93c94037e3c8898edaa1f0db14232b1f5785
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-01-13 12:58:05 +0300

    updated src/gendiff.js on step#5

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,14 +1,17 @@
 import _ from 'lodash';
+import parse from './parsers.js';
 
-const genDiff = (data1, data2) => {
-  if (data1 === '{}' && data2 === '{}') {
-    return '{}';
-  }
-  const obj1 = JSON.parse(data1);
-  const obj2 = JSON.parse(data2);
+const genDiff = (filepath1, filepath2) => {
+  const obj1 = parse(filepath1);
+  const obj2 = parse(filepath2);
+
   const keys1 = _.keys(obj1);
   const keys2 = _.keys(obj2);
   const keys = _.union(keys1, keys2);
+
+  if (keys.length === 0) {
+    return 'no differences';
+  }
 
   const differences = keys
     .sort()
