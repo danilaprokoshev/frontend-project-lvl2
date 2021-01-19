549540253557a42155f47f7d5114adc952044a2a
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-01-19 10:33:50 +0300

    updated bin/gendiff.js on check#0

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,6 +9,7 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
+  .allowUnknownOption()
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
     const absolutePath1 = path.resolve(filepath1);
