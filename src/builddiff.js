d5488ee70f3624591f940ff429e07e0c9b592f57
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-01-19 21:14:15 +0300

    updated src/builddiff on check#1

--- src/builddiff.js
+++ src/builddiff.js
@@ -7,32 +7,32 @@ const buildDiff = (obj1, obj2) => {
   const sortedKeys = _.sortBy(keys);
 
   return sortedKeys
-    .reduce((acc, key) => {
+    .map((key) => {
       if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
         const children = buildDiff(obj1[key], obj2[key]);
 
-        return [...acc, { key, children }];
+        return { key, children };
       }
       if (!_.has(obj1, key)) {
-        return [...acc, {
+        return {
           key, value: obj2[key], status: 'added',
-        }];
+        };
       }
       if (!_.has(obj2, key)) {
-        return [...acc, {
+        return {
           key, value: obj1[key], status: 'deleted',
-        }];
+        };
       }
-      if (obj1[key] !== obj2[key]) {
-        return [...acc, {
+      if (JSON.stringify(obj1[key]) !== JSON.stringify(obj2[key])) {
+        return {
           key, value: obj2[key], previousValue: obj1[key], status: 'changed',
-        }];
+        };
       }
 
-      return [...acc, {
+      return {
         key, value: obj1[key], status: 'unchanged',
-      }];
-    }, []);
+      };
+    });
 };
 
 export default buildDiff;
