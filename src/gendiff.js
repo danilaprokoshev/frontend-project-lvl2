cf3eb54d4e35dbfdb4a6b04b8c052d5a0e0e299d
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-01-19 14:33:35 +0300

    updated src/gendiff.js on check#0

--- src/gendiff.js
+++ src/gendiff.js
@@ -6,35 +6,34 @@ const buildDiff = (obj1, obj2) => {
   const keys1 = _.keys(obj1);
   const keys2 = _.keys(obj2);
   const keys = _.union(keys1, keys2);
-  const sortedKeys = keys.sort();
+  const sortedKeys = _.sortBy(keys);
 
   return sortedKeys
     .reduce((acc, key) => {
-      let temp = acc;
       if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
         const children = buildDiff(obj1[key], obj2[key]);
-        temp = _.concat(temp, { key, children });
-        return temp;
+
+        return [...acc, { key, children }];
       }
       if (!_.has(obj1, key)) {
-        temp = _.concat(temp, {
+        return [...acc, {
           key, value: obj2[key], status: 'added',
-        });
-      } else if (!_.has(obj2, key)) {
-        temp = _.concat(temp, {
+        }];
+      }
+      if (!_.has(obj2, key)) {
+        return [...acc, {
           key, value: obj1[key], status: 'deleted',
-        });
-      } else if (obj1[key] !== obj2[key]) {
-        temp = _.concat(temp, {
+        }];
+      }
+      if (obj1[key] !== obj2[key]) {
+        return [...acc, {
           key, value: obj2[key], previousValue: obj1[key], status: 'changed',
-        });
-      } else {
-        temp = _.concat(temp, {
-          key, value: obj1[key], status: 'unchanged',
-        });
+        }];
       }
 
-      return temp;
+      return [...acc, {
+        key, value: obj1[key], status: 'unchanged',
+      }];
     }, []);
 };
 
