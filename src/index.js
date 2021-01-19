f81693f48116bc2a75cffeaf9b75e0b29d4fdea3
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-01-19 21:15:06 +0300

    updated src/index.js on check#1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+import path from 'path';
+import fs from 'fs';
 import parse from './parsers.js';
-import format from '../formatters/index.js';
+import format from './formatters/index.js';
 import buildDiff from './builddiff.js';
 
 export default (filepath1, filepath2, formatName = 'stylish') => {
-  const obj1 = parse(filepath1);
-  const obj2 = parse(filepath2);
+  const extName1 = path.extname(filepath1);
+  const data1 = fs.readFileSync(filepath1, 'utf-8');
+
+  const extName2 = path.extname(filepath2);
+  const data2 = fs.readFileSync(filepath2, 'utf-8');
+
+  const obj1 = parse(data1, extName1);
+  const obj2 = parse(data2, extName2);
 
   // TODO: реализовать проверку на пустые файлы и соответствующий возврат
   if (Object.keys(obj1).length === 0 && Object.keys(obj2).length === 0) {
