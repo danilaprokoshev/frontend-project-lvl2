917cdaf5e57848823d973c45e924f4588f50d1cd
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-01-22 17:16:03 +0300

    updated src/formatters/stylish.js on check#3

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,22 +1,23 @@
 import _ from 'lodash';
 
+const indent = (count) => '  '.repeat(count);
+
 const printObject = (object, depth) => {
-  const repeat = (counter) => '  '.repeat(counter + 2);
   const iter = (innerObject, innerDepth) => {
     const entries = Object.entries(innerObject);
     return entries
       .map((entry) => {
         const [key, value] = entry;
         if (_.isPlainObject(value)) {
-          return `${repeat(innerDepth)}${key}: {\n${iter(value, innerDepth + 2)}\n${repeat(innerDepth)}}`;
+          return `${indent(innerDepth)}    ${key}: {\n${iter(value, innerDepth + 2)}\n    ${indent(innerDepth)}}`;
         }
 
-        return `${repeat(innerDepth)}${key}: ${value}`;
+        return `${indent(innerDepth)}    ${key}: ${value}`;
       })
       .join('\n');
   };
 
-  return `{\n${iter(object, depth)}\n${'  '.repeat(depth)}}`;
+  return `{\n${iter(object, depth)}\n${indent(depth)}}`;
 };
 
 const stylish = (diff) => {
@@ -25,18 +26,18 @@ const stylish = (diff) => {
       if (node.type !== 'nest') {
         switch (node.type) {
           case 'added':
-            return `${'  '.repeat(depth)}+ ${node.key}: ${(_.isPlainObject(node.value)) ? printObject(node.value, depth + 1) : node.value}`;
+            return `${indent(depth)}+ ${node.key}: ${(_.isPlainObject(node.value)) ? printObject(node.value, depth + 1) : node.value}`;
           case 'deleted':
-            return `${'  '.repeat(depth)}- ${node.key}: ${(_.isPlainObject(node.value)) ? printObject(node.value, depth + 1) : node.value}`;
+            return `${indent(depth)}- ${node.key}: ${(_.isPlainObject(node.value)) ? printObject(node.value, depth + 1) : node.value}`;
           case 'changed':
-            return `${'  '.repeat(depth)}- ${node.key}: ${(_.isPlainObject(node.previousValue)) ? printObject(node.previousValue, depth + 1) : node.previousValue}\n${'  '.repeat(depth)}+ ${node.key}: ${(_.isPlainObject(node.value)) ? printObject(node.value, depth + 1) : node.value}`;
+            return `${indent(depth)}- ${node.key}: ${(_.isPlainObject(node.previousValue)) ? printObject(node.previousValue, depth + 1) : node.previousValue}\n${indent(depth)}+ ${node.key}: ${(_.isPlainObject(node.value)) ? printObject(node.value, depth + 1) : node.value}`;
           case 'unchanged':
-            return `${'  '.repeat(depth)}  ${node.key}: ${node.value}`;
+            return `${indent(depth)}  ${node.key}: ${node.value}`;
           default:
         }
       }
 
-      return `${'  '.repeat(depth + 1)}${node.key}: {\n${iter(node.children, depth + 2)}\n${'  '.repeat(depth + 1)}}`;
+      return `${indent(depth)}  ${node.key}: {\n${iter(node.children, depth + 2)}\n  ${indent(depth)}}`;
     })
     .join('\n');
 
