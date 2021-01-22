eac02d4f279c89520a15460acf578c78e7d1b8e0
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-01-22 11:57:14 +0300

    updated src/index.js on check#2

--- src/index.js
+++ src/index.js
@@ -5,19 +5,16 @@ import format from './formatters/index.js';
 import buildDiff from './builddiff.js';
 
 export default (filepath1, filepath2, formatName = 'stylish') => {
-  const extName1 = path.extname(filepath1);
-  const data1 = fs.readFileSync(filepath1, 'utf-8');
+  const absolutePath1 = path.resolve(filepath1);
+  const absolutePath2 = path.resolve(filepath2);
+  const dataType1 = path.extname(absolutePath1).slice(1);
+  const data1 = fs.readFileSync(absolutePath1, 'utf-8');
 
-  const extName2 = path.extname(filepath2);
-  const data2 = fs.readFileSync(filepath2, 'utf-8');
+  const dataType2 = path.extname(absolutePath2).slice(1);
+  const data2 = fs.readFileSync(absolutePath2, 'utf-8');
 
-  const obj1 = parse(data1, extName1);
-  const obj2 = parse(data2, extName2);
-
-  // TODO: реализовать проверку на пустые файлы и соответствующий возврат
-  if (Object.keys(obj1).length === 0 && Object.keys(obj2).length === 0) {
-    return 'no differences';
-  }
+  const obj1 = parse(data1, dataType1);
+  const obj2 = parse(data2, dataType2);
 
   const diff = buildDiff(obj1, obj2);
 
