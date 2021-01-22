e9483f2fb4bf850bd05615a2f76d9ede52acd313
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-01-22 11:59:31 +0300

    updated src/parsers.js on check#2

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,10 @@
 import yaml from 'js-yaml';
 
-const parse = (data, extName) => {
-  switch (extName) {
-    case '.json':
+const parse = (data, dataType) => {
+  switch (dataType) {
+    case 'json':
       return JSON.parse(data);
-    case '.yml':
+    case 'yml':
       return yaml.load(data);
     default:
       throw new Error("Unexpected file's extension");
