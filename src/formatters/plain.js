8edf5dd532a2f82ecb8c6a57ede74cb74483a7f7
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-01-25 16:23:43 +0300

    updated src/formatters/plain.js on check#5

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -15,18 +15,19 @@ const printValue = (value) => {
 const plain = (diff) => {
   const iter = (tree, path) => tree
     .map((node) => {
-      if (node.type === 'unchanged') {
-        return null;
-      }
       if (node.type === 'nest') {
         return `${iter(node.children, path.concat(node.key, '.'))}`;
       }
-      if (node.type === 'added') {
-        return `Property '${path.join('')}${node.key}' was added with value: ${printValue(node.value)}`;
+      switch (node.type) {
+        case 'unchanged':
+          return null;
+        case 'added':
+          return `Property '${path.join('')}${node.key}' was added with value: ${printValue(node.value)}`;
+        case 'deleted':
+          return `Property '${path.join('')}${node.key}' was removed`;
+        default:
+          return `Property '${path.join('')}${node.key}' was updated. From ${printValue(node.previousValue)} to ${printValue(node.value)}`;
       }
-      return (node.type === 'deleted')
-        ? `Property '${path.join('')}${node.key}' was removed`
-        : `Property '${path.join('')}${node.key}' was updated. From ${printValue(node.previousValue)} to ${printValue(node.value)}`;
     })
     .filter((n) => n)
     .join('\n');
