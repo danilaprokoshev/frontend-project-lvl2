b296c86ffcb8e6d8ece6cb212b45242ba2f7f9b6
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-01-27 00:40:40 +0300

    updated src/index.js on check#5

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import parse from './parsers.js';
 import format from './formatters/index.js';
 import buildDiff from './builddiff.js';
 
-export default (filepath1, filepath2, formatName = 'stylish') => {
-  const absolutePath1 = path.resolve(filepath1);
-  const absolutePath2 = path.resolve(filepath2);
-  const dataType1 = path.extname(absolutePath1).slice(1);
-  const data1 = fs.readFileSync(absolutePath1, 'utf-8');
+const readFileAndGetType = (filepath) => {
+  const absolutePath = path.resolve(filepath);
+  const data = fs.readFileSync(absolutePath, 'utf-8');
+  const dataType = path.extname(absolutePath).slice(1);
 
-  const dataType2 = path.extname(absolutePath2).slice(1);
-  const data2 = fs.readFileSync(absolutePath2, 'utf-8');
+  return [data, dataType];
+};
+
+export default (filepath1, filepath2, formatName = 'stylish') => {
+  const [data1, dataType1] = readFileAndGetType(filepath1);
+  const [data2, dataType2] = readFileAndGetType(filepath2);
 
   const obj1 = parse(data1, dataType1);
   const obj2 = parse(data2, dataType2);
